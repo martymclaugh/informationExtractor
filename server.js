9a473f7b75eb2590fa7cb66495c11b559b26a73b
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-20 13:50:08 -0800

    set scrape route with parameter

--- server.js
+++ server.js
@@ -2,3 +2,12 @@ var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
+
+app.get('/scrape/:movieId', function(req, res){
+  console.log(req.params.movieId);
+})
+
+app.listen('8081');
+console.log('Scraping on port 8081');
+
+module.exports = app
